6e740ee98cfb8065463bcf170edcf6d7d165feb9
diegomedina62 <diegomedina62@gmail.com>
2022-09-05 21:20:22 -0300

    asyncWrapper and test on postman

--- controllers/characters.js
+++ controllers/characters.js
@@ -1,14 +1,22 @@
 const { createCustomError } = require("../errors/custom-error");
 const { StatusCodes } = require("http-status-codes");
+const asyncWrapper = require("../middlewares/asyncWrapper");
 
 const getAllCharacters = (req, res) => {
   res.send("get all Characters");
 };
 
-const getCharacter = (req, res) => {
+const getCharacter = asyncWrapper((req, res, next) => {
   const { name } = req.params;
+  if (name == "error") {
+    throw createCustomError(
+      "Error Test/prueba de error",
+      StatusCodes.IM_A_TEAPOT
+    );
+  }
   res.send(`info about ${name}`);
-};
+});
+
 const createCharacter = (req, res) => {
   const data = req.body;
   res.json({ "Requested Route": "create Character", data });
