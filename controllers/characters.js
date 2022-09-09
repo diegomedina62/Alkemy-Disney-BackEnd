4c4d0e674dd66033a74ffd4d7750e525807de63a
diegomedina62 <diegomedina62@gmail.com>
2022-09-09 09:40:42 -0300

    createCharacter on Character controller

--- controllers/characters.js
+++ controllers/characters.js
@@ -1,7 +1,12 @@
+const { Sequelize } = require("sequelize");
 const { createCustomError } = require("../errors/custom-error");
 const { StatusCodes } = require("http-status-codes");
 const asyncWrapper = require("../middlewares/asyncWrapper");
 
+//importing models
+const DB = require("../SQLdatabase")();
+
+//controllers
 const getAllCharacters = (req, res) => {
   res.send("get all Characters");
 };
@@ -11,10 +16,29 @@ const getCharacter = asyncWrapper((req, res, next) => {
   res.send(`info about ${name}`);
 });
 
-const createCharacter = (req, res) => {
-  const data = req.body;
-  res.json({ "Requested Route": "create Character", data });
-};
+const createCharacter = asyncWrapper(async (req, res, next) => {
+  const { moviesAndSeries, ...characterData } = req.body;
+  const character = await DB.Character.build(characterData);
+  //case with input with no associated Movies
+  if (!moviesAndSeries) {
+    character.save();
+    return res
+      .status(StatusCodes.OK)
+      .json({ "Character created": characterData });
+  }
+
+  //case with associated Movie
+  const movie = await DB.Movie.findOne({ where: { title: moviesAndSeries } });
+  if (!movie) {
+    await character.save();
+    await character.createMovie({ title: moviesAndSeries });
+  } else {
+    await character.save();
+    await character.addMovie(movie);
+  }
+  res.status(StatusCodes.OK).send("Character created with movie added");
+});
+
 const updateCharacter = (req, res) => {
   const { name } = req.params;
   const data = req.body;
