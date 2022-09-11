631ca084d642fa5d2c68f35380991ab1f6dc4fb5
diegomedina62 <diegomedina62@gmail.com>
2022-09-11 13:37:45 -0300

    Movies controllers and movie models modifications

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,24 +1,151 @@
-const getAllMovies = (req, res) => {
-  res.send("get all movies");
-};
+const { createCustomError } = require("../errors/custom-error");
+const { StatusCodes } = require("http-status-codes");
+const asyncWrapper = require("../middlewares/asyncWrapper");
 
-const getMovie = (req, res) => {
+//importing models
+const Character = require("../models/characters");
+const Movies = require("../models/movies");
+const Gender = require("../models/gender");
+const sequelize = require("../database/db");
+8;
+const getAllMovies = asyncWrapper(async (req, res, next) => {
+  let searchOptions = { attributes: ["title", "image", "filmDate"] };
+  const { title, order, queryGender } = req.query;
+  if (title) {
+    searchOptions.where = { title };
+  }
+  if (order) {
+    searchOptions.order = [["title", order]];
+  }
+
+  if (queryGender) {
+    const gender = await Gender.findByPk(queryGender);
+    if (!gender) {
+      throw createCustomError("Gender doesn't exist", StatusCodes.BAD_REQUEST);
+    }
+    const result = await gender.getMovies(searchOptions);
+    return res.status(StatusCodes.OK).json({
+      msg: "List of Movies",
+      result,
+      searchOptions,
+    });
+  }
+
+  const result = await Movies.findAll(searchOptions);
+
+  res
+    .status(StatusCodes.OK)
+    .json({ msg: "List of Movies", result, searchOptions });
+});
+
+const getMovie = asyncWrapper(async (req, res, next) => {
   const { title } = req.params;
-  res.send(`info about ${title}`);
-};
-const createMovie = (req, res) => {
-  data = req.body;
-  res.json({ "requested Route": "Create Movie", data });
-};
-const updateMovie = (req, res) => {
+
+  const movie = await Movies.findByPk(title);
+  if (!movie) {
+    throw createCustomError("Movie doesn't exist", StatusCodes.BAD_REQUEST);
+  }
+
+  const AssociatedCharaters = (
+    await movie.getCharacters({ attributes: ["name"] })
+  ).map((x) => x.name);
+  const AssociatedGenders = (
+    await movie.getGenders({
+      attributes: ["gender"],
+    })
+  ).map((x) => x.gender);
+
+  res.status(StatusCodes.OK).json({
+    msg: "Movie Information",
+    result: { movie, AssociatedCharaters, AssociatedGenders },
+  });
+});
+
+const createMovie = asyncWrapper(async (req, res, next) => {
+  const { associatedGender, associatedCharacter, ...movieData } = req.body;
+
+  const result = await sequelize.transaction(async (t) => {
+    const movie = await Movies.create(movieData, { transaction: t });
+    let character = "";
+    let charCreated = "";
+    let gender = "";
+    let genderCreated = "";
+    if (associatedGender) {
+      [gender, genderCreated] = await Gender.findOrCreate({
+        where: { gender: associatedGender },
+        transaction: t,
+      });
+      await movie.addGender(gender, { transaction: t });
+    }
+    if (associatedCharacter) {
+      [character, charCreated] = await Character.findOrCreate({
+        where: { name: associatedCharacter },
+        transaction: t,
+      });
+      await movie.addCharacter(character, { transaction: t });
+    }
+
+    return { movie, character, charCreated, gender, genderCreated };
+  });
+
+  res.status(StatusCodes.OK).json({
+    msg: "Create Movie",
+    result,
+  });
+});
+
+const updateMovie = asyncWrapper(async (req, res, next) => {
   const { title } = req.params;
-  const data = req.body;
-  res.send({ "Requested Route": `update ${title}`, data });
-};
-const deleteMovie = (req, res) => {
+  const { associatedGender, associatedCharacter, ...movieData } = req.body;
+
+  let movie = await Movies.findByPk(title);
+  if (!movie) {
+    throw createCustomError(
+      "Movie does not exist. Create one using createMovie",
+      StatusCodes.BAD_REQUEST
+    );
+  }
+
+  const result = await sequelize.transaction(async (t) => {
+    let character = "";
+    let charCreated = "";
+    let gender = "";
+    let genderCreated = "";
+    if (associatedGender) {
+      [gender, genderCreated] = await Gender.findOrCreate({
+        where: { gender: associatedGender },
+        transaction: t,
+      });
+      await movie.addGender(gender, { transaction: t });
+    }
+    if (associatedCharacter) {
+      [character, charCreated] = await Character.findOrCreate({
+        where: { name: associatedCharacter },
+        transaction: t,
+      });
+      await movie.addCharacter(character, { transaction: t });
+    }
+
+    movie.set(movieData);
+    movie = await movie.save({ transaction: t });
+
+    return { movie, character, charCreated, gender, genderCreated };
+  });
+
+  res.status(StatusCodes.OK).json({ msg: "Movie Updated ", result });
+});
+
+const deleteMovie = asyncWrapper(async (req, res, next) => {
   const { title } = req.params;
-  res.send(`delete ${title}`);
-};
+  let movie = await Movies.findByPk(title);
+  if (!movie) {
+    throw createCustomError("Movie does not exist", StatusCodes.BAD_REQUEST);
+  }
+
+  await movie.destroy();
+
+  res.status(StatusCodes.OK).json({ msg: `Movie Deleted: ${title}` });
+});
 
 module.exports = {
   getAllMovies,
--- models/movies.js
+++ models/movies.js
@@ -8,8 +8,21 @@ const Movies = sequelize.define(
       type: DataTypes.STRING,
       primaryKey: true,
     },
-    imagen: DataTypes.BLOB,
-    fecha: DataTypes.DATE,
+    image: DataTypes.BLOB,
+    filmDate: {
+      type: DataTypes.DATEONLY,
+      set(value) {
+        //create date Object
+        this.setDataValue("filmDate", new Date().setFullYear(value, 1, 1));
+      },
+      get() {
+        if (!this.getDataValue("filmDate")) {
+          return null;
+        }
+        const rawValue = new Date(this.getDataValue("filmDate"));
+        return rawValue.getFullYear();
+      },
+    },
     rating: {
       type: DataTypes.INTEGER,
       validate: {
